c1210a0ec69bea40c3a11a6b602d998b6d886be2
5up3r22 <juandavidocampo80@gmail.com>
2023-05-18 20:49:05 -0500

    actualizacion Bd

--- Database/superalitas.sql
+++ Database/superalitas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-05-2023 a las 02:37:47
--- Versión del servidor: 10.4.27-MariaDB
--- Versión de PHP: 8.2.0
+-- Tiempo de generación: 19-05-2023 a las 03:48:22
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,7 +29,86 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `estados` (
   `id` int(11) NOT NULL,
-  `descripcion` varchar(40) NOT NULL
+  `descripcion` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mesas`
+--
+
+CREATE TABLE `mesas` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(45) NOT NULL,
+  `ubicacion` varchar(45) NOT NULL,
+  `estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mesas_reserva`
+--
+
+CREATE TABLE `mesas_reserva` (
+  `id` int(11) NOT NULL,
+  `reserva` int(11) NOT NULL,
+  `mesa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reserva`
+--
+
+CREATE TABLE `reserva` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `fecha_solicitud` datetime NOT NULL,
+  `fecha_reserva` datetime NOT NULL,
+  `observaciones_usuario` varchar(60) NOT NULL,
+  `costo_reserva` double NOT NULL,
+  `estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_usuario`
+--
+
+CREATE TABLE `tipo_usuario` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tipo_usuario`
+--
+
+INSERT INTO `tipo_usuario` (`id`, `descripcion`) VALUES
+(1, 'SUPER ADMIN'),
+(2, 'ADMIN'),
+(3, 'CLIENTE');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombres` varchar(45) NOT NULL,
+  `documento` varchar(45) NOT NULL,
+  `correo` varchar(45) NOT NULL,
+  `contrasena` varchar(45) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `telefono` varchar(45) NOT NULL,
+  `id_tipo_usuario` int(11) NOT NULL,
+  `estado` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -43,6 +122,43 @@ ALTER TABLE `estados`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `mesas`
+--
+ALTER TABLE `mesas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_estados` (`estado`);
+
+--
+-- Indices de la tabla `mesas_reserva`
+--
+ALTER TABLE `mesas_reserva`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_id_reserva` (`reserva`),
+  ADD KEY `fk_id_mesa` (`mesa`);
+
+--
+-- Indices de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_id_usuario` (`id_usuario`),
+  ADD KEY `fk_id_estado` (`estado`);
+
+--
+-- Indices de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_tipo_usuario` (`id_tipo_usuario`),
+  ADD KEY `fk_id_estados` (`estado`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -51,6 +167,67 @@ ALTER TABLE `estados`
 --
 ALTER TABLE `estados`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `mesas`
+--
+ALTER TABLE `mesas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `mesas_reserva`
+--
+ALTER TABLE `mesas_reserva`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `mesas`
+--
+ALTER TABLE `mesas`
+  ADD CONSTRAINT `fk_estados` FOREIGN KEY (`estado`) REFERENCES `estados` (`id`);
+
+--
+-- Filtros para la tabla `mesas_reserva`
+--
+ALTER TABLE `mesas_reserva`
+  ADD CONSTRAINT `fk_id_mesa` FOREIGN KEY (`mesa`) REFERENCES `mesas` (`id`),
+  ADD CONSTRAINT `fk_id_reserva` FOREIGN KEY (`reserva`) REFERENCES `reserva` (`id`);
+
+--
+-- Filtros para la tabla `reserva`
+--
+ALTER TABLE `reserva`
+  ADD CONSTRAINT `fk_id_estado` FOREIGN KEY (`estado`) REFERENCES `estados` (`id`),
+  ADD CONSTRAINT `fk_id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `fk_id_estados` FOREIGN KEY (`estado`) REFERENCES `estados` (`id`),
+  ADD CONSTRAINT `fk_tipo_usuario` FOREIGN KEY (`id_tipo_usuario`) REFERENCES `tipo_usuario` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
