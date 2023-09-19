ac7ba2c5b0528f671125727e1e3ad8c68388c268
JuDaOcVa <juandavidocampo80@gmail.com>
2023-09-19 10:30:05 -0500

    Actualizacion migracion de bd a postgres con los datos que ya existian

--- Database/superalitas.sql
+++ Database/superalitas.sql
@@ -1,283 +1,104 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 27-05-2023 a las 16:40:57
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Crear base de datos
+--CREATE DATABASE superalitas;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Conectar a la base de datos
+\c superalitas;
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `superalitas`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `estados`
---
-
-CREATE TABLE `estados` (
-  `id` int(11) NOT NULL,
-  `descripcion` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `estados`
---
-
-INSERT INTO `estados` (`id`, `descripcion`) VALUES
-(1, 'ACTIVO'),
-(2, 'INACTIVO');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `mesas`
---
-
-CREATE TABLE `mesas` (
-  `id` int(11) NOT NULL,
-  `descripcion` varchar(45) NOT NULL,
-  `ubicacion` varchar(45) NOT NULL,
-  `estado` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `mesas`
---
-
-INSERT INTO `mesas` (`id`, `descripcion`, `ubicacion`, `estado`) VALUES
-(1, 'Mesa 1', 'Lado de Caja', 1),
-(2, 'Mesa 2', 'Al lado del estante', 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `mesas_reserva`
---
-
-CREATE TABLE `mesas_reserva` (
-  `id` int(11) NOT NULL,
-  `reserva` int(11) NOT NULL,
-  `mesa` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `mesas_reserva`
---
-
-INSERT INTO `mesas_reserva` (`id`, `reserva`, `mesa`) VALUES
-(1, 11, 1),
-(2, 12, 1),
-(3, 13, 1),
-(4, 14, 1),
-(5, 15, 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `reserva`
---
-
-CREATE TABLE `reserva` (
-  `id` int(11) NOT NULL,
-  `id_usuario` int(11) NOT NULL,
-  `fecha_solicitud` date NOT NULL,
-  `hora_solicitud` time NOT NULL,
-  `fecha_reserva` date NOT NULL,
-  `hora_reserva` time NOT NULL,
-  `observaciones_usuario` varchar(60) NOT NULL,
-  `costo_reserva` double NOT NULL,
-  `estado` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `reserva`
---
-
-INSERT INTO `reserva` (`id`, `id_usuario`, `fecha_solicitud`, `hora_solicitud`, `fecha_reserva`, `hora_reserva`, `observaciones_usuario`, `costo_reserva`, `estado`) VALUES
-(11, 2, '2023-05-21', '22:33:17', '2023-05-22', '22:30:00', 'Prueba 1', 0, 1),
-(12, 2, '2023-05-21', '22:37:26', '2023-05-22', '23:40:00', 'Prueba 2', 0, 1),
-(13, 2, '2023-05-21', '22:38:40', '2023-05-22', '22:40:00', 'Prueba 3', 0, 1),
-(14, 2, '2023-05-26', '20:29:16', '2023-05-27', '20:30:00', 'Reserva actualizada', 0, 1),
-(15, 2, '2023-05-26', '22:46:00', '2023-05-29', '21:30:00', 'Reserva familiar', 0, 1);
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `tipo_usuario`
---
+CREATE TABLE tipo_usuario (
+  id serial PRIMARY KEY,
+  descripcion varchar(45) NOT NULL
+);
 
-CREATE TABLE `tipo_usuario` (
-  `id` int(11) NOT NULL,
-  `descripcion` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
 -- Volcado de datos para la tabla `tipo_usuario`
---
+INSERT INTO tipo_usuario (descripcion) VALUES
+('ADMIN'),
+('EMPLEADOS'),
+('CLIENTE');
 
-INSERT INTO `tipo_usuario` (`id`, `descripcion`) VALUES
-(1, 'ADMIN'),
-(2, 'EMPLEADOS'),
-(3, 'CLIENTE');
+-- Estructura de tabla para la tabla `estados`
+CREATE TABLE estados (
+  id serial PRIMARY KEY,
+  descripcion varchar(45) NOT NULL
+);
 
--- --------------------------------------------------------
+-- Volcado de datos para la tabla `estados`
+INSERT INTO estados (descripcion) VALUES
+('ACTIVO'),
+('INACTIVO');
 
---
+-- Estructura de tabla para la tabla `mesas`
+CREATE TABLE mesas (
+  id serial PRIMARY KEY,
+  descripcion varchar(45) NOT NULL,
+  ubicacion varchar(45) NOT NULL,
+  estado int NOT NULL,
+  FOREIGN KEY (estado) REFERENCES estados (id)
+);
+
+-- Volcado de datos para la tabla `mesas`
+INSERT INTO mesas (descripcion, ubicacion, estado) VALUES
+('Mesa 1', 'Lado de Caja', 1),
+('Mesa 2', 'Al lado del estante', 1);
+
 -- Estructura de tabla para la tabla `usuarios`
---
+CREATE TABLE usuarios (
+  id serial PRIMARY KEY,
+  nombres varchar(45) NOT NULL,
+  documento varchar(45) NOT NULL,
+  correo varchar(45) NOT NULL,
+  contrasena varchar(45) NOT NULL,
+  fecha_nacimiento date NOT NULL,
+  telefono varchar(45) NOT NULL,
+  id_tipo_usuario int NOT NULL,
+  estado int NOT NULL,
+  estado_sesion int NOT NULL,
+  FOREIGN KEY (id_tipo_usuario) REFERENCES tipo_usuario (id),
+  FOREIGN KEY (estado) REFERENCES estados (id),
+  FOREIGN KEY (estado_sesion) REFERENCES estados (id)
+);
 
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nombres` varchar(45) NOT NULL,
-  `documento` varchar(45) NOT NULL,
-  `correo` varchar(45) NOT NULL,
-  `contrasena` varchar(45) NOT NULL,
-  `fecha_nacimiento` date NOT NULL,
-  `telefono` varchar(45) NOT NULL,
-  `id_tipo_usuario` int(11) NOT NULL,
-  `estado` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
 -- Volcado de datos para la tabla `usuarios`
---
+INSERT INTO usuarios (nombres, documento, correo, contrasena, fecha_nacimiento, telefono, id_tipo_usuario, estado, estado_sesion) VALUES
+('Luis Alejandro Alvarez', '123456789', 'luisalejandro@gmail.com', '12345', '2001-05-10', '12345', 3, 1, 1),
+('Juan David Ocampo Valencia', '1006009546', 'juandavidocampo80@gmail.com', '12345', '2001-12-26', '3137784186', 3, 1, 1);
 
-INSERT INTO `usuarios` (`id`, `nombres`, `documento`, `correo`, `contrasena`, `fecha_nacimiento`, `telefono`, `id_tipo_usuario`, `estado`) VALUES
-(2, 'Luis Alejandro Alvarez', '123456789', 'luisalejandro@gmail.com', '12345', '2001-05-10', '12345', 3, 1),
-(3, 'Juan David Ocampo Valencia', '1006009546', 'juandavidocampo80@gmail.com', '12345', '2001-12-26', '3137784186', 3, 1);
+-- Estructura de tabla para la tabla `reserva`
+CREATE TABLE reserva (
+  id serial PRIMARY KEY,
+  id_usuario int NOT NULL,
+  fecha_solicitud date NOT NULL,
+  hora_solicitud time NOT NULL,
+  fecha_reserva date NOT NULL,
+  hora_reserva time NOT NULL,
+  observaciones_usuario varchar(60) NOT NULL,
+  costo_reserva double precision NOT NULL,
+  estado int NOT NULL,
+  FOREIGN KEY (id_usuario) REFERENCES usuarios (id),
+  FOREIGN KEY (estado) REFERENCES estados (id)
+);
 
---
--- Índices para tablas volcadas
---
+-- Volcado de datos para la tabla `reserva`
+INSERT INTO reserva (id_usuario, fecha_solicitud, hora_solicitud, fecha_reserva, hora_reserva, observaciones_usuario, costo_reserva, estado) VALUES
+(2, '2023-05-21', '22:33:17', '2023-05-22', '22:30:00', 'Prueba 1', 0, 1),
+(2, '2023-05-21', '22:37:26', '2023-05-22', '23:40:00', 'Prueba 2', 0, 1),
+(2, '2023-05-21', '22:38:40', '2023-05-22', '22:40:00', 'Prueba 3', 0, 1),
+(2, '2023-05-26', '20:29:16', '2023-05-27', '20:30:00', 'Reserva actualizada', 0, 1),
+(2, '2023-05-26', '22:46:00', '2023-05-29', '21:30:00', 'Reserva familiar', 0, 1);
 
---
--- Indices de la tabla `estados`
---
-ALTER TABLE `estados`
-  ADD PRIMARY KEY (`id`);
+-- Estructura de tabla para la tabla `mesas_reserva`
+CREATE TABLE mesas_reserva (
+  id serial PRIMARY KEY,
+  reserva int NOT NULL,
+  mesa int NOT NULL,
+  FOREIGN KEY (reserva) REFERENCES reserva (id),
+  FOREIGN KEY (mesa) REFERENCES mesas (id)
+);
 
---
--- Indices de la tabla `mesas`
---
-ALTER TABLE `mesas`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_estados` (`estado`);
-
---
--- Indices de la tabla `mesas_reserva`
---
-ALTER TABLE `mesas_reserva`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_id_reserva` (`reserva`),
-  ADD KEY `fk_id_mesa` (`mesa`);
-
---
--- Indices de la tabla `reserva`
---
-ALTER TABLE `reserva`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_id_usuario` (`id_usuario`),
-  ADD KEY `fk_id_estado` (`estado`);
-
---
--- Indices de la tabla `tipo_usuario`
---
-ALTER TABLE `tipo_usuario`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_tipo_usuario` (`id_tipo_usuario`),
-  ADD KEY `fk_id_estados` (`estado`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `estados`
---
-ALTER TABLE `estados`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de la tabla `mesas`
---
-ALTER TABLE `mesas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de la tabla `mesas_reserva`
---
-ALTER TABLE `mesas_reserva`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT de la tabla `reserva`
---
-ALTER TABLE `reserva`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
---
--- AUTO_INCREMENT de la tabla `tipo_usuario`
---
-ALTER TABLE `tipo_usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `mesas`
---
-ALTER TABLE `mesas`
-  ADD CONSTRAINT `fk_estados` FOREIGN KEY (`estado`) REFERENCES `estados` (`id`);
-
---
--- Filtros para la tabla `mesas_reserva`
---
-ALTER TABLE `mesas_reserva`
-  ADD CONSTRAINT `fk_id_mesa` FOREIGN KEY (`mesa`) REFERENCES `mesas` (`id`),
-  ADD CONSTRAINT `fk_id_reserva` FOREIGN KEY (`reserva`) REFERENCES `reserva` (`id`);
-
---
--- Filtros para la tabla `reserva`
---
-ALTER TABLE `reserva`
-  ADD CONSTRAINT `fk_id_estado` FOREIGN KEY (`estado`) REFERENCES `estados` (`id`),
-  ADD CONSTRAINT `fk_id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
-
---
--- Filtros para la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD CONSTRAINT `fk_id_estados` FOREIGN KEY (`estado`) REFERENCES `estados` (`id`),
-  ADD CONSTRAINT `fk_tipo_usuario` FOREIGN KEY (`id_tipo_usuario`) REFERENCES `tipo_usuario` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Volcado de datos para la tabla `mesas_reserva`
+INSERT INTO mesas_reserva (reserva, mesa) VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 1);
